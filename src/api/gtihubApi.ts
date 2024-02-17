2fc2e6f79d649dc70a22f27c4e7886e6f2961777
afgomezv <afgomezvonline@gmail.com>
2024-02-17 10:42:28 -0500

    seccion 7 finalizada

--- src/api/gtihubApi.ts
+++ src/api/gtihubApi.ts
@@ -4,6 +4,6 @@ export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
     Authorization:
-      "Bearer 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",
+      "Bearer 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",
   },
 });
---
+++ src/issues/hooks/useIssuesInifinte.tsx
@@ -0,0 +1,58 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Issue, State } from "../interfaces";
+import { sleep } from "../../helpers";
+import { githubApi } from "../../api/gtihubApi";
+import { get } from "http";
+
+interface Props {
+  state?: State;
+  labels: string[];
+  page?: number;
+}
+
+interface QueryProps {
+  pageParam?: number;
+  queryKey: (string | Props)[];
+}
+
+const getIssues = async ({
+  pageParam = 1,
+  queryKey,
+}: QueryProps): Promise<Issue[]> => {
+  const [, , args] = queryKey;
+  const { state, labels } = args as Props;
+
+  sleep(2);
+
+  const params = new URLSearchParams();
+
+  if (state) params.append("state", state);
+
+  if (labels?.length > 0) {
+    const labelString = labels.join(",");
+    params.append("labels", labelString);
+  }
+
+  params.append("page", pageParam.toString());
+  params.append("per_page", "5");
+
+  const { data } = await githubApi.get<Issue[]>("/issues", { params });
+  //console.log(data);
+  return data;
+};
+
+export const useIssuesInifinte = ({ labels, state }: Props) => {
+  const issuesQuery = useInfiniteQuery({
+    queryKey: ["issues", "infinite", { labels, state }],
+    queryFn: (data) => getIssues(data),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.length === 0) return;
+
+      return pages.length + 1;
+    },
+  });
+  return {
+    issuesQuery,
+  };
+};
